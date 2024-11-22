6196ef40c6a8943237f5adb592539e01d487f967
MG10327 <54483612+MG10327@users.noreply.github.com>
2024-11-22 01:59:05 -0500

    add cards

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
 import "./TitleCards.scss"
+import cards_data from '../../assets/cards/Cards_data'
 
 const TitleCards = () => {
   return (
-    <div>TitleCards</div>
+    <div className='titlecards'>
+      <h2>Popular on Netflix</h2>
+      <div className="card-list">
+        {cards_data.map((card, index) =>
+          <div className='card' key={index}>
+            <img src={card.image} alt={card.name} />
+            <p>{card.name}</p>
+          </div>
+        )}
+      </div>
+    </div>
   )
 }
 
