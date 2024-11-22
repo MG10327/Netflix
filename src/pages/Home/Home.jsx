a12038498b44a7c5861e9192f2c853d27a5417ae
MG10327 <54483612+MG10327@users.noreply.github.com>
2024-11-22 01:32:42 -0500

    basic hero styles

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,6 +3,8 @@ import "./Home.scss"
 import Navbar from '../../components/Navbar/Navbar'
 import hero_banner from "../../assets/hero_banner.jpg"
 import hero_title from "../../assets/hero_title.png"
+import play_icon from "../../assets/play_icon.png"
+import info_icon from "../../assets/info_icon.png"
 
 const Home = () => {
   return (
@@ -13,6 +15,10 @@ const Home = () => {
             <div className="hero-caption">
                 <img src={hero_title} alt="" className='caption-img' />
                 <p>Discovering his ties to a secret ancient order, a young man liging in modern Istanbul embarks on a quest to save the city from an immortal enemy.</p>
+                <div className="hero-btns">
+                    <button className='btn'><img src={play_icon} alt="Play" />Play</button>
+                    <button className='btn dark-btn'><img src={info_icon} alt="More info" />More</button>
+                </div>
             </div>
         </div>
     </div>
