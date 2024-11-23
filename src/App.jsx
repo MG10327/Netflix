de59075bf6173c47628cc5cecb7ead11bab7f082
MG10327 <54483612+MG10327@users.noreply.github.com>
2024-11-22 19:35:10 -0500

    add login and logout to the page with navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import {Routes, Route} from "react-router-dom"
+import React, { useEffect } from 'react'
+import {Routes, Route, useNavigate} from "react-router-dom"
 import Home from './pages/Home/Home'
 import Login from "./pages/Login/Login"
 import Player from "./pages/Player/Player"
+import { onAuthStateChanged } from 'firebase/auth'
+import { auth } from '../firebase'
 
 const App = () => {
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    onAuthStateChanged(auth, async (user) => {
+      if(user) {
+        console.log("Loggged In")
+        navigate('/') // This sends users to the home page when they're logged in.
+      } else {
+        console.log("Logged Out")
+        navigate('/login') // This sends users to the login page when they logout
+      }
+    })
+  }, []) // This makes sure the function only gets called on app mount.
+
+
   return (
     <div>
       <Routes>
