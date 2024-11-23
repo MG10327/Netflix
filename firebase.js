25242641d4e29af06c7251a3a21f78973bdca517
MG10327 <54483612+MG10327@users.noreply.github.com>
2024-11-22 19:11:53 -0500

    setup basic auth functions

---
+++ firebase.js
@@ -0,0 +1,51 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import getAuth, { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from "firebase/auth"
+import getFireStore, { addDoc, collection } from "firebase/firestore"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAZQ6X-zad2AK_QWVafRJnWPZhFKCVR7HE",
+  authDomain: "netflix-55477.firebaseapp.com",
+  projectId: "netflix-55477",
+  storageBucket: "netflix-55477.firebasestorage.app",
+  messagingSenderId: "891338665362",
+  appId: "1:891338665362:web:f8056840eccb4219cbe54b",
+  measurementId: "G-259ZLB753K"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app)
+const db = getFireStore(app)
+
+const signup = async (name, email, password) => {
+    try {
+        const response =await createUserWithEmailAndPassword(auth, email, password)
+
+        const user = response.user
+        await addDoc(collection(db, "user"), {
+            uid: user.uid,
+            name: name,
+            authProvider: "local",
+            email: email
+        })
+    } catch(error) {
+        console.log(error)
+        alert(error)
+    }
+}
+
+const login = async (email, password) => {
+    try {
+       await signInWithEmailAndPassword(auth, email, password)
+    } catch(error) {
+        console.log(error)
+        alert(error)
+    }
+}
+
+const logout = () => {
+    signOut(auth)
+}
+
+export {auth, db, login, signup, logout}
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import "./Login.scss"
 import logo from "../../assets/logo.png"
+import {login, signup} from "../../firebase"
+
 
 const Login = () => {
 
